164809923a8abbeec7bde0bf37d3661189f6e8ef
Sam Wells <35074076+abandofmilk@users.noreply.github.com>
2021-10-09 14:42:15 -0400

    Update DiceRollFrequency.cpp
    Removed unnecessary store variable from generateDiceRolls() function
    Removed the vector clear() and renamed the output function.

--- DiceRollFrequency.cpp
+++ DiceRollFrequency.cpp
@@ -7,29 +7,26 @@ std::map<int, int> output{};
 
 void generateDieRolls(const std::vector<int>& dice, const std::vector<int>::size_type index = 0, int sum = 0)
 {
-    int store = sum;
     for(int i = 1; i <= dice[index]; ++i)
     {
-        sum += i;
+        ++sum;
         if(index < dice.size() - 1)
             generateDieRolls(dice, index + 1, sum);
         else
             ++output[sum];
-        sum = store;
     }
 }
 
-void printOutputAndClear()
+void printOutput()
 {
     for(const auto p : output)
         std::cout << std::setw(3) << p.first << ": " << std::string(p.second, '+') << '\n';
     std::cout << '\n';
-    output.clear();
 }
 
 int main()
 {
     // 3d6
     generateDieRolls({6,6,6});
-    printOutputAndClear();
+    printOutput();
 }
